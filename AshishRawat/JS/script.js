b3d21aaf882cd31bd29bb988f0a9fbb61bc20387
Ashish Rawat <ashish1198.cse16@chitkara.edu.in>
2019-09-14 18:01:49 +0530

    Text field updated

--- AshishRawat/JS/script.js
+++ AshishRawat/JS/script.js
@@ -1,6 +1,7 @@
 var box=0;
 var count=0;
 var m=9;
+var bt;
 async function clicked(elementbyid)
 {
 var con = document.getElementById(elementbyid).innerHTML;
@@ -11,16 +12,20 @@ var con = document.getElementById(elementbyid).innerHTML;
         
           box=1;
           document.getElementById(elementbyid).innerHTML="X";
+          bt=document.getElementById(elementbyid).innerHTML;
+
           count++;
           m--;
           document.getElementById(elementbyid).setAttribute("data-points","-1");
           document.getElementById("sc").value=m;
+
 
       }
       else if(box==1)
       {
         box=0;
           document.getElementById(elementbyid).innerHTML="O";
+          bt=document.getElementById(elementbyid).innerHTML;
           count++;
           m--;
           document.getElementById(elementbyid).setAttribute("data-points","1");
@@ -44,19 +49,20 @@ for (var i = 0; i < 3; i++)
 { 
 for (var j = 0; j < 3; j++) 
 { 
-    arr[i][j] = 0; 
+    arr[i][j] = 0;
 } 
 }
 /*-----------------------------------------JQUERY_ANIMATE-----------------------------------------------*/
 $(document).ready(function(){
     var div = $("table");
-    var btn= $(".btn")
+    var btn= $(".btn");
+
     div.animate({height: '300px', opacity: '0.2'}, "slow");
     div.animate({width: '300px', opacity: '0.6'}, "slow");
     btn.animate({height: '90px', opacity: '0.2'}, "slow");
     btn.animate({width: '90px', opacity: '0.6'}, "slow");
-    //div.animate({height: '100px', opacity: '0.4'}, "slow");
-    //div.animate({width: '100px', opacity: '0.8'}, "slow");
+    bt.css("color", "white").fadeOut(250).fadeIn(10);
+    
     });
 /*------------------------------------------------------------------------------------------------------*/
 
