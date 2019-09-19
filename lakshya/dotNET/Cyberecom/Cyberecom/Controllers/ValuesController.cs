53c489223b2b0880ec9bf1250d610da06e57d7bd
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-09-19 20:42:22 +0530

    Api created again

---
+++ lakshya/dotNET/Cyberecom/Cyberecom/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cyberecom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cyberecom.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        E_CommContext dc = new E_CommContext();
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var a = dc.Product.ToList();
+            return a;
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            var n = dc.Product.Find(id);
+            return n;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Product value)
+        {
+            dc.Product.Add(value);
+            dc.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var obj = dc.Product.Find(id);
+            dc.Product.Remove(obj);
+            dc.SaveChanges();
+        }
+
+    }
+}
---
+++ lakshya/dotNET/Cyberecom/Cyberecom/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cyberecom.Models
+{
+    public partial class Brand
+    {
+        public int Pid { get; set; }
+        public int Bid { get; set; }
+        public string Bname { get; set; }
+
+        public Brand P { get; set; }
+        public Brand InverseP { get; set; }
+    }
+}
