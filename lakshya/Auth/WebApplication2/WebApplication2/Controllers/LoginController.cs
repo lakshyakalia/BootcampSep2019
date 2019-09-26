d54a29ffef3d5f3ff493c4a810907664ea8f28f0
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-09-26 13:00:25 +0530

    Passing values from header

--- lakshya/Auth/WebApplication2/WebApplication2/Controllers/LoginController.cs
+++ lakshya/Auth/WebApplication2/WebApplication2/Controllers/LoginController.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
-    
+
     [Produces("application/json")]
     [Route("api/Login")]
     public class LoginController : Controller
@@ -27,36 +28,60 @@ namespace WebApplication2.Controllers
         {
             return "value";
         }
-        
+
         // POST: api/Login
         [HttpPost]
-        public IActionResult Post([FromBody]Signn value)
+        public IActionResult Post([FromBody]dynamic value)
         {
+            StringValues emailValue;
+            StringValues passwordValue;
+            Request.Headers.TryGetValue("username", out emailValue);
+            Request.Headers.TryGetValue("password", out passwordValue);
+            String username = emailValue.FirstOrDefault();
+            String password = passwordValue.FirstOrDefault();
+
+            Signn loggedinUser = obj.Signn.Find(username);
             try
             {
-                var val = obj.Signn.Where(em => em.Username == value.Username).ToList();
-                if (val.Count() > 0)
+                if (loggedinUser.Password.Equals(password))
                 {
-                    if (val.Exists(Pass => string.Compare(Pass.Password, value.Password) == 0))
-                        return Ok(val);
-                    else
-                        return BadRequest("Wrong Password!");
+                    return Ok(true);
                 }
-                else
-                    return BadRequest("Username Invalid"); 
             }
-            catch(Exception e)
+            catch (Exception ex)
             {
-                return BadRequest(e);
+                return Unauthorized();
             }
+            return BadRequest();
         }
-        
+
+
+        //try
+        //{
+        //    var val = obj.Signn.Where(em => em.Username == value.Username).ToList();
+        //    if (val.Count() > 0)
+        //    {
+        //        if (val.Exists(Pass => string.Compare(Pass.Password, value.Password) == 0))
+        //            return Ok(val);
+        //        else
+        //            return BadRequest("Wrong Password!");
+        //    }
+        //    else
+        //        return BadRequest("Username Invalid"); 
+        //}
+        //catch(Exception e)
+        //{
+        //    return BadRequest(e);
+        //}
+
+
+
         // PUT: api/Login/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-        
+
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
@@ -64,3 +89,4 @@ namespace WebApplication2.Controllers
         }
     }
 }
+
