43198e300896f29d2c9cb07a25c65141d0153915
birendra-bit <bhujelbirendra3@gmail.com>
2019-10-27 01:19:08 +0530

    show test and show question are made in single file

---
+++ Birdendra/exam-portal/frontend/public/js/show-questions.js
@@ -0,0 +1,12 @@
+// this page will create dom to display questions
+var questions = [{
+    question :'Question-',
+    questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
+    option1 : 'option1',
+    option2 : 'option2',
+    option3 : 'option3',
+    option4 : 'option4'
+}]
+function editQuestion(){
+    
+}
