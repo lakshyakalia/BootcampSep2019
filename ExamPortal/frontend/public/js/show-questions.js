864357d4610546296de09526ad73b7e08027bf6a
birendra-bit <bhujelbirendra3@gmail.com>
2019-10-29 18:15:54 +0530

    frontend files are modified

--- ExamPortal/frontend/public/js/show-questions.js
+++ ExamPortal/frontend/public/js/show-questions.js
@@ -1,72 +1,92 @@
 // this page will create dom to display questions
+// this page will create dom to display questions
 var questions = [{
     question :'Question-',
     questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
-    options = [
+    options :[
         'option1','option2','option3','option4'
-    ]
-}]
+    ],
+    answer :'option1'
+},
+{
+    question :'Question-',
+    questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
+    options :[
+        'option1','option2','option3','option4'
+    ],
+    answer :'option3'
+},
+{
+    question :'Question-',
+    questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
+    options :[
+        'option1','option2','option3','option4'
+    ],
+    answer :'option4'
+}
+]
+
+
  function editQuestion(){
-    console.log(questions)
 
-    // for ( let i = 0; i< questions.length; i++ ){
+    for ( let i = 0; i< questions.length; i++ ){
 
-    //     let rowDiv = document.createElement('div')
+        let anchorTag = document.createElement('a')
+        anchorTag.href ='#question'+i
+        anchorTag.innerHTML = questions[i].question+eval(i+1)
+        $("#question-Index").append(anchorTag)
 
-    //     let col1 = document.createElement('div')
-    //     col1.className = 'col-md-2'
-    //     let anchorTag = document.createElement('a')
-    //     anchorTag.href ='#question'+i
-    //     anchorTag.innerHTML = questions[i].question+i+1
-    //     col1.append(anchorTag)
-    //     rowDiv.append(col1)
+        let br = document.createElement('br')
+        $("#question-Index").append(br)
 
-    //     let col2 = document.createElement('div')
-    //     col2.className = 'col-md-10'
+        let innerRowDiv = document.createElement('div')
+        innerRowDiv.className ='row'
+        $("#question-Display").append(innerRowDiv)
 
-    //     let innerRowDiv = document.createElement('div')
-    //     innerRowDiv.className ='row'
+        let innerCol1 = document.createElement('div')
+        innerCol1.className = 'col-md-12 question-div'
+        innerCol1.id = 'question'+i
+        innerRowDiv.append(innerCol1)
 
-    //     let innerCol1 = document.createElement('div')
-    //     innerCol1.className = 'col-md-12 question-div'
-    //     innerCol1.id = 'question'+i
+        let h3 = document.createElement('h3')
+        h3.innerHTML = questions[i].question + eval(i+1)
+        innerCol1.append(h3)
 
-    //     let h3 = document.createElement('h3')
-    //     h3.innerHTML = questions[i].question +i+1
-    //     innerCol1.append(h3)
+        let para = document.createElement('p')
+        para.innerHTML = questions[i].questionText
+        innerCol1.append(para)
 
-    //     let para = document.createElement('p')
-    //     para.innerHTML = questions[i].questionText
-    //     innerCol1.append(para)
+        let ul = document.createElement('ul')
+        ul.setAttribute('list-style-type','circle')
 
-    //     let ul = document.createElement('ul')
-    //     ul.setAttribute('list-style-type','circle')
+        for ( let j = 0; j < questions[i].options.length; j++ ){
+            let li = document.createElement('li')
+            li.innerHTML = questions[i].options[j]
+            ul.append(li)
+        }
+        innerCol1.append(ul)
 
-    //     for ( let j = 0; j < questions[i].options.length; j++ ){
-    //         let li = document.createElement('li')
-    //         li.innerHTML = questions[i].options[j]
-    //         ul.append(li)
-    //     }
-    //     innerCol1.append(ul)
+        let h6 = document.createElement('h6')
+        h6.innerHTML = 'Answer : '+questions[i].answer
+        innerCol1.append(h6)
 
-    //     let editBtn = document.createElement('button')
-    //     editBtn.type = 'button'
-    //     editBtn.id ='edit'+i
-    //     editBtn.className = 'btn btn-outline-info'
-    //     editBtn.innerHTML ='Edit'
-    //     innerCol1.append(editBtn)
+        let btnDiv = document.createElement('div')
+        btnDiv.className = 'btn1'
 
-    //     let deleteBtn = document.createElement('button')
-    //     deleteBtn.type ='button'
-    //     deleteBtn.className = 'btn btn-outline-danger'
-    //     deleteBtn.id ='del'+i
-    //     deleteBtn.innerHTML = 'Delete'
-    //     innerCol1.append(deleteBtn)
+        let editBtn = document.createElement('button')
+        editBtn.type = 'button'
+        editBtn.id ='edit'+i
+        editBtn.className = 'btn btn-outline-info'
+        editBtn.innerHTML ='Edit'
+        btnDiv.append(editBtn)
+        innerCol1.append(btnDiv)
 
-    //     innerRowDiv.append(innerCol1)
-    //     col2.append(innerCol1)
-    //     rowDiv.append(col2)
-
-    //     $('#mainDiv').append(rowDiv)
-    // }
+        let deleteBtn = document.createElement('button')
+        deleteBtn.type ='button'
+        deleteBtn.className = 'btn btn-outline-danger'
+        deleteBtn.id ='del'+i
+        deleteBtn.innerHTML = 'Delete'
+        btnDiv.append(deleteBtn)
+        innerCol1.append(btnDiv)
+    }
 }
--- ExamPortal/frontend/public/js/show-test.js
+++ ExamPortal/frontend/public/js/show-test.js
@@ -10,89 +10,6 @@ var data =[{
     }
 ]
 
-// this page will create dom to display questions
-var questions = [{
-    question :'Question-',
-    questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
-    options :[
-        'option1','option2','option3','option4'
-    ]
-},
-{
-    question :'Question-',
-    questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
-    options :[
-        'option1','option2','option3','option4'
-    ]
-},
-{
-    question :'Question-',
-    questionText : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illum velit blanditiis',
-    options :[
-        'option1','option2','option3','option4'
-    ]
-}
-]
- function editQuestion(){
-
-    for ( let i = 0; i< questions.length; i++ ){
-
-        let anchorTag = document.createElement('a')
-        anchorTag.href ='#question'+i
-        anchorTag.innerHTML = questions[i].question+eval(i+1)
-        $("#question-Index").append(anchorTag)
-
-        let br = document.createElement('br')
-        $("#question-Index").append(br)
-
-        let innerRowDiv = document.createElement('div')
-        innerRowDiv.className ='row'
-        $("#question-Display").append(innerRowDiv)
-
-        let innerCol1 = document.createElement('div')
-        innerCol1.className = 'col-md-12 question-div'
-        innerCol1.id = 'question'+i
-        innerRowDiv.append(innerCol1)
-
-        let h3 = document.createElement('h3')
-        h3.innerHTML = questions[i].question + eval(i+1)
-        innerCol1.append(h3)
-
-        let para = document.createElement('p')
-        para.innerHTML = questions[i].questionText
-        innerCol1.append(para)
-
-        let ul = document.createElement('ul')
-        ul.setAttribute('list-style-type','circle')
-
-        for ( let j = 0; j < questions[i].options.length; j++ ){
-            let li = document.createElement('li')
-            li.innerHTML = questions[i].options[j]
-            ul.append(li)
-        }
-        innerCol1.append(ul)
-
-        let btnDiv = document.createElement('div')
-        btnDiv.className = 'btn1'
-
-        let editBtn = document.createElement('button')
-        editBtn.type = 'button'
-        editBtn.id ='edit'+i
-        editBtn.className = 'btn btn-outline-info'
-        editBtn.innerHTML ='Edit'
-        btnDiv.append(editBtn)
-        innerCol1.append(btnDiv)
-
-        let deleteBtn = document.createElement('button')
-        deleteBtn.type ='button'
-        deleteBtn.className = 'btn btn-outline-danger'
-        deleteBtn.id ='del'+i
-        deleteBtn.innerHTML = 'Delete'
-        btnDiv.append(deleteBtn)
-        innerCol1.append(btnDiv)
-    }
-}
-
 function showQuestion(id){
     // let testCode = $('#'+id).parent().prev().prev().find('p').html()
     // console.log(testCode)
@@ -102,9 +19,12 @@ function showQuestion(id){
     editQuestion()
 }
 function deleteTest(id){
-    let testCode = $('#'+id).parent().prev().prev().prev().find('p').html()
+    let testCode = $('#'+id).parent().prev().prev().prev().prev().find('p').html()
     console.log(testCode)
 }
+
+
+
 $(document).ready(()=>{
     let parent = $(".test-detail")
 //create DOM to display tests as per its need
@@ -113,7 +33,7 @@ $(document).ready(()=>{
         row.className = 'row content'
 
         let col1 = document.createElement('div')
-        col1.className = 'col-md-3'
+        col1.className = 'col-md-2'
         let p1 = document.createElement('p')
         p1.innerHTML = data[i].testName
         col1.append(p1);
@@ -127,7 +47,7 @@ $(document).ready(()=>{
         row.append(col2)
 
         let col3 = document.createElement('div')
-        col3.className = 'col-md-3'
+        col3.className = 'col-md-2'
         let p3 = document.createElement('p')
         p3.innerHTML = data[i].testDate
         col3.append(p3)
@@ -146,15 +66,25 @@ $(document).ready(()=>{
 
         let col5 = document.createElement('div')
         col5.className = 'col-md-2'
+        let ebtn = document.createElement('button')
+        ebtn.id = 'editTest'+i
+        ebtn.className = 'btn btn-outline-primary'
+        ebtn.innerHTML ='Edit Test'
+        col5.append(ebtn)
+        row.append(col5)
+
+        let col6 = document.createElement('div')
+        col6.className = 'col-md-2'
         let btn2 = document.createElement('button')
         btn2.type = 'button'
         btn2.id = 'delete'+i
         btn2.className = 'btn btn-outline-danger'
         btn2.setAttribute('onclick','deleteTest(this.id)')
         btn2.innerHTML = 'Delete'
-        col5.append(btn2)
-        row.append(col5)
+        col6.append(btn2)
+        row.append(col6)
 
         parent.append(row)
     }
+    
 })
