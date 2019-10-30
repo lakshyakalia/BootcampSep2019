984d87579ce0ce6ba0379ffed874874dd73e726c
Garima Sharma <gasharma.cse16@chitkarauniversity.edu.in>
2019-10-30 18:31:55 +0530

    stepper added

--- ExamPortal/frontend/public/js/examdetails.js
+++ ExamPortal/frontend/public/js/examdetails.js
@@ -1,66 +1,43 @@
-$(document).ready(function() {
-    $('.loader').hide()
-    document.getElementById('btnSave').addEventListener('click', validateForm)
-
-    function validateForm() {
-        var testName = document.getElementById("addquestionTestname").value;
-        var testCode = document.getElementById("addquestionTestcode").value;
-        var testDuration = document.getElementById("addquestionTestduration").value;
-        var testInstruction = document.getElementById("addquestionTestinstruction").value;
-        if (testName === "") {
-            alert("Please enter test name");
-        } else {
-                testName = true;
-            }
-
-        if (testCode === "") {
-            alert("Please enter test code");
-        } else {
-            
-                testCode = true;
-            }
-
-        if (testDuration === "") {
-            alert("Please enter test duration");
-        } else {
-           
-                testDuration = true;
-            }
-
-        if (testInstruction == "") {
-            alert("Please enter test instruction");
-        } else {
-            
-                testInstruction = true;
-
-            }
-
-
-        if ((testName || testCode || testDuration || testInstruction) == true) {
-            let testData = {
-                testName: $('#addquestionTestname').val(),
-                testCode: $('#addquestionTestcode').val(),
-                testDuration: $('#addquestionTestduration').val(),
-                testInstruction: $('#addquestionTestinstruction').val(),
-                
-            }
-            $.ajax("http://localhost:9000/exam", {
-                type: "POST",
-                dataType: "json",
-
-                contentType: "application/json;charset=utf-8",
-
-                data: JSON.stringify(testData),
-                contentType: "application/json; charset=utf-8",
-                success: function(data, status) {
-                    document.getElementById('show-messages').innerHTML = "Details Added"
-                },
-                error: function(error) {
-                    console.log("error : " + error)
-                }
-            })
-        } else {
-            return false;
+$(document).ready(function () {
+    var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+  
+    allWells.hide();
+  
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+  
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
         }
-    }
-})
+    });
+  
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+  
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+  
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+  
+    $('div.setup-panel div a.btn-primary').trigger('click');
+  });
+ 
