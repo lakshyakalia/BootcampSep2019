b42e415f3ed10833e46f2d378292904628414c01
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-10-31 17:52:41 +0530

    updated view performance and examiner.html

---
+++ ExamPortal/backend/controller/studentPerformance.js
@@ -0,0 +1,26 @@
+const { examDetail } = require('../Models/examDetail')
+
+
+const viewPerformance = async(req, res) => {
+    try {
+        const obj = new Object()
+            // let arr = []
+            // arr.push(obj)
+        obj.id = '1'
+        obj.name = 'Birendra'
+        obj.testId = '1011'
+        obj.attempted = '10'
+        obj.correct = '5'
+        obj.unattempted = '0'
+        obj.totalScore = '5'
+        obj.percentage = '50%'
+
+
+        res.send(obj)
+    } catch (error) {
+        res.send(error)
+    }
+}
+module.exports = {
+    viewPerformance
+}
