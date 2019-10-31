ba0159ccd107ab22dd084137e2be7d528d60b455
birendra-bit <bhujelbirendra3@gmail.com>
2019-10-31 18:21:41 +0530

    edit question function added

---
+++ birendra/API_project/API_project/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_project.Models
+{
+    public partial class Brand
+    {
+        public int BId { get; set; }
+        public int? BName { get; set; }
+        public int? PId { get; set; }
+    }
+}
