c6a923e9ee400b90303dacd22ec033e31845de24
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-11-01 12:20:41 +0530

    examiner.js created

---
+++ ExamPortal/frontend/public/js/examiner.js
@@ -0,0 +1,23 @@
+function showEdit() {
+    document.getElementById('showEditDiv').style.display = 'block';
+
+    $.ajax("http://localhost:3000/examiner/:" + id, {
+        type: 'GET',
+        dataType: 'JSON',
+
+        success: function(data) {
+            // data = JSON.stringify(data)
+            let indexTemplate = $("#view-student-performance").html();
+            // $.each(data, (index, item) => {
+            // console.log(item.name)
+            // console.log(indexTemplate)
+            $("#tbdy").append(Mustache.render(indexTemplate, data))
+                // })
+        },
+        error: function(error) {}
+    })
+}
+
+function editDetails() {
+
+}
