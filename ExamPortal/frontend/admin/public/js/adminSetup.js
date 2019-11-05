324daef3990b334dd14d13654d19a5518b12a051
Chandan <ch.cse16@chitkarauniversity.edu.in>
2019-11-05 10:28:21 +0530

    changes in form validation

--- ExamPortal/frontend/admin/public/js/adminSetup.js
+++ ExamPortal/frontend/admin/public/js/adminSetup.js
@@ -1,5 +1,6 @@
+
 $(document).ready(function(){
-    $("#submit").click(function(e){
+     $("#submit").click(function(e){
         e.preventDefault();
         var email=document.getElementById("email").value;
         var name=document.getElementById("name").value;
@@ -7,39 +8,66 @@ $(document).ready(function(){
         var accountype=document.getElementById("accountype").value;
         var phoneno=document.getElementById("phoneno").value;
         var collegename=document.getElementById("collegename").value;
-
-        console.log(email);
-        console.log(name);
-        console.log(password);
-        console.log(accountype);
-        console.log(phoneno);
-        console.log(collegename);
-       
-      $.ajax("http://127.0.0.1:3000/signup",{
-        type:"POST",
-        dataType:"json",
-        contentType:"application/json",
+       // var a=phoneno.toString().length;
+        //console.log(a);
+         if(email==="")
+         {
+            window.alert("Email must be added");
+         }
+         else if(name==="")
+         {
+          window.alert("Name must be added");
+         }
+         else if(password==="")
+         {
+          window.alert("Password must be added");
+         }
+         else if((phoneno.toString().length !=10))
+         {
+          window.alert("Phoneno must be valid");
+         }
+         else if(collegename==="")
+         {
+          window.alert("College Name must be added");
+         }
         
-            data:JSON.stringify(
-                {
-                  "email":email,
-                  "name":name,
-                  "password":password,
-                  "accountType":accountype,
-                  "collegeName":collegename,
-                  "phoneNumber":phoneno
+        // &&(phoneno.toString().length>9)
+         //console.log();
+        // console.log(email);
+        // console.log(name);
+        // console.log(password);
+        // console.log(accountype);
+        // console.log(phoneno);
+        // console.log(collegename);
+        if(!email==""&&!name==""&&!password==""&&!phoneno==""&&!collegename=="")
+        {
+          console.log("hello buddy");
+      // $.ajax("http://127.0.0.1:3000/signup",{
+      //   type:"POST",
+      //   dataType:"json",
+      //   contentType:"application/json",
+        
+      //       data:JSON.stringify(
+      //           {
+      //             "email":email,
+      //             "name":name,
+      //             "password":password,
+      //             "accountType":accountype,
+      //             "collegeName":collegename,
+      //             "phoneNumber":phoneno
 
-                }
-            ),
-            success:function(recent){ 
-              console.log("Data inserted");
-              window.location.replace("adminHome.html")
-            },
-            error:function()
-            {
-                console.log("Something went wrong");
-            }
+      //           }
+      //       ),
+      //       success:function(recent){ 
+      //         console.log("Data inserted");
+      //         window.location.replace("adminHome.html")
+      //       },
+      //       error:function()
+      //       {
+      //           console.log("Something went wrong");
+      //       }
             
-          });
-      });
+      //     });
+        }
+       });
   });
