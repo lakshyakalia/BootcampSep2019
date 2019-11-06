587628819d33891034f659352de116eea1e481f0
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-11-07 00:40:42 +0530

    loader functionality running by setTimeout

--- ExamPortal/frontend/user/public/js/login.js
+++ ExamPortal/frontend/user/public/js/login.js
@@ -1,33 +1,38 @@
 $(document).on('click', '#loginButton', function() {
-    let email = $('#inputEmail').val()
-    let password = $('#inputPassword').val()
-    $.ajax('http://localhost:3000/login', {
-        type: 'POST',
-        dataType: 'JSON',
-        beforeSend: function() {
-            $('.spinner').show()
-        },
-        complete: function() {
-            $('.spinner').hide()
-        },
-        data: {
-            'email': email,
-            'password': password
-        },
-        success: function(data) {
-            localStorage.setItem('token', data.token)
-                //here i have used accountType to redirect to respective page
-            if (data.accountType == "Examiner")
-                $(location).attr('href', '../../exminer/views/examiner.html')
-            else if (data.accountType == "Student")
-                $(location).attr('href', './accessKey.html')
-            else {
-                $(location).attr('href', '../../admin/views/adminHome.html')
+    $('.main').animate({ opacity: 0.4 })
+    $('.mod').fadeIn()
+    $('.spinner').show()
+    timeout = setTimeout(function() {
+        let email = $('#inputEmail').val()
+        let password = $('#inputPassword').val()
+        $.ajax('http://localhost:3000/login', {
+            type: 'POST',
+            dataType: 'JSON',
+            // for loader
+            // beforeSend: function() {
+            //     $('body').animate({ opacity: 0.6 })
+            //     $('#myModal').fadeIn()
+            //     $('.spinner').show()
+            // },
+            data: {
+                'email': email,
+                'password': password
+            },
+            success: function(data) {
+                localStorage.setItem('token', data.token)
+                    //here i have used accountType to redirect to respective page
+                if (data.accountType == "Examiner")
+                    $(location).attr('href', '../../exminer/views/examiner.html')
+                else if (data.accountType == "Student")
+                    $(location).attr('href', './accessKey.html')
+                else {
+                    $(location).attr('href', '../../admin/views/adminHome.html')
+                }
+            },
+            error: function(error) {
+                console.log(error)
             }
-        },
-        error: function(error) {
-            console.log(error)
-        }
 
-    })
+        })
+    }, 1000)
 })
