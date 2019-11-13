702eda3cd1af6b1a3e17c9d09b2a04c0c1f5d3aa
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-11-13 16:02:45 +0530

    dotnet folder added

---
+++ ExamPortal/backend/backend_dotnet/Examportal/Custom Models/UserLoginCustomModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examportal.Custom_Models
+{
+    public class UserLoginCustomModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ExamPortal/backend/backend_dotnet/Examportal/Models/ExamportalContext.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Examportal.Models
+{
+    public partial class ExamportalContext : DbContext
+    {
+        public ExamportalContext()
+        {
+        }
+
+        public ExamportalContext(DbContextOptions<ExamportalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CandidateAnswer> CandidateAnswer { get; set; }
+        public virtual DbSet<ExamDetails> ExamDetails { get; set; }
+        public virtual DbSet<Questions> Questions { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public object SignUpCustomModel { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=CYG238;Database=Examportal;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CandidateAnswer>(entity =>
+            {
+                entity.HasKey(e => e.Email);
+
+                entity.ToTable("candidateAnswer");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CompletionTime)
+                    .HasColumnName("completionTime")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CorrectStatus).HasColumnName("correctStatus");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("createdBy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("createdDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("modifiedBy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modifiedDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.QuestionId).HasColumnName("questionId");
+
+                entity.Property(e => e.SubmitExam).HasColumnName("submitExam");
+
+                entity.Property(e => e.TestCode)
+                    .HasColumnName("testCode")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TotalScore).HasColumnName("totalScore");
+
+                entity.HasOne(d => d.EmailNavigation)
+                    .WithOne(p => p.CandidateAnswer)
+                    .HasForeignKey<CandidateAnswer>(d => d.Email)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__candidate__email__04E4BC85");
+
+                entity.HasOne(d => d.Question)
+                    .WithMany(p => p.CandidateAnswer)
+                    .HasForeignKey(d => d.QuestionId)
+                    .HasConstraintName("FK__candidate__quest__03F0984C");
+            });
+
+            modelBuilder.Entity<ExamDetails>(entity =>
+            {
+                entity.HasKey(e => e.ExamCode);
+
+                entity.ToTable("examDetails");
+
+                entity.Property(e => e.ExamCode)
+                    .HasColumnName("examCode")
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("createdBy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("createdDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ExamDuration)
+                    .HasColumnName("examDuration")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ExamName)
+                    .HasColumnName("examName")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ExamStartTime)
+                    .HasColumnName("examStartTime")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("modifiedBy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modifiedDate")
+                    .HasColumnType("date");
+
+                entity.HasOne(d => d.EmailNavigation)
+                    .WithMany(p => p.ExamDetails)
+                    .HasForeignKey(d => d.Email)
+                    .HasConstraintName("FK__examDetai__email__6B24EA82");
+            });
+
+            modelBuilder.Entity<Questions>(entity =>
+            {
+                entity.HasKey(e => e.QuestionId);
+
+                entity.ToTable("questions");
+
+                entity.Property(e => e.QuestionId).HasColumnName("questionId");
+
+                entity.Property(e => e.Answer)
+                    .HasColumnName("answer")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AnswerType)
+                    .HasColumnName("answerType")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("createdBy")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("createdDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.ExamCode)
+                    .HasColumnName("examCode")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("modifiedBy")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modifiedDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Option1)
+                    .HasColumnName("option1")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Option2)
+                    .HasColumnName("option2")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Option3)
+                    .HasColumnName("option3")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Option4)
+                    .HasColumnName("option4")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.QuestionImage)
+                    .HasColumnName("questionImage")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.QuestionText)
+                    .HasColumnName("questionText")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Weightage).HasColumnName("weightage");
+
+                entity.HasOne(d => d.ExamCodeNavigation)
+                    .WithMany(p => p.Questions)
+                    .HasForeignKey(d => d.ExamCode)
+                    .HasConstraintName("FK__questions__examC__7B5B524B");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.Email);
+
+                entity.ToTable("users");
+
+                entity.HasIndex(e => e.PhoneNumber)
+                    .HasName("UQ__users__4849DA0175A5ACF1")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.AccountType)
+                    .HasColumnName("accountType")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CollegeName)
+                    .HasColumnName("collegeName")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("createdBy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("createdDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("modifiedBy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modifiedDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasColumnName("phoneNumber")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
