f36e3245da0644f1877e4946b41ca12d93ea797c
birendra-bit <bhujelbirendra3@gmail.com>
2019-11-13 20:36:50 +0530

    dot net folder structure

---
+++ ExamPortal/backend/backend_dotnet/Examportal/Controllers/examDetailsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examportal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Examportal.Controllers
+{
+    [Route("[controller]")]
+    public class examDetailsController : ControllerBase
+    {
+        public IConfiguration _config;
+        public examDetailsController(IConfiguration config)
+        {
+            _config = config;
+        }
+        ExamportalContext db = new ExamportalContext();
+        
+        [Route("/exam")]
+        [HttpPost]
+        public IActionResult<ExamDetails> saveExam([FromBody] ExamDetails examDetails)
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                
+            }
+            return BadRequest();
+        }
+    }
+}
