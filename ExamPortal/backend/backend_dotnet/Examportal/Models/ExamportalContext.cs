08b7540f03fd0f0aecb3f1ffaca4abda544f25c5
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2019-11-22 11:19:47 +0530

    files updated final

--- ExamPortal/backend/backend_dotnet/Examportal/Models/ExamportalContext.cs
+++ ExamPortal/backend/backend_dotnet/Examportal/Models/ExamportalContext.cs
@@ -26,7 +26,7 @@ namespace Examportal.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=CYG238;Database=Examportal;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=CYG355;Database=Examportal;Trusted_Connection=True;");
             }
         }
 
